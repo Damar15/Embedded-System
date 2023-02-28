406957b018967fd32f00810979fe1f3e76c88f8c
Dani Mahardika <46182715+Damar15@users.noreply.github.com>
2023-02-28 11:58:03 +0700

    Add files via upload

---
+++ Code/Percobaan 1/Percobaan1.3/Percobaan1.3.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensorValue = analogRead(A2);
+  Serial.println(sensorValue);
+  digitalWrite(8, HIGH);
+  delay(sensorValue);
+  digitalWrite(8, LOW);
+  delay(sensorValue);
+}
